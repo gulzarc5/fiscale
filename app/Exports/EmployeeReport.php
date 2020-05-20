ea5f34e86c7e1c7eb61d3ac8b06e303ca1b691e8
gulzarc5 <gulzarc5@gmail.com>
2020-05-20 21:12:27 +0530

    member job_report error corrected

--- app/Exports/EmployeeReport.php
+++ app/Exports/EmployeeReport.php
@@ -59,7 +59,7 @@ class EmployeeReport implements FromArray,ShouldAutoSize,WithEvents
         
      
         $job = DB::table('job')
-            ->select('job.*','client.client_id as c_id','client.name as c_name','client.pan as c_pan','job_type.name as job_type_name','branch.name as branch_name')
+            ->select('job.*','client.name as c_name','client.pan as c_pan','job_type.name as job_type_name','branch.name as branch_name')
             ->leftjoin('client','client.id','=','job.client_id')
             ->leftjoin('branch','branch.id','=','job.created_by_id')
             ->leftjoin('job_type','job_type.id','=','job.job_type')
